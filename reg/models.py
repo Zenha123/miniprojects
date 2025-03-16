d6ab9b13c22b1a22f03a4c53e5363efacc17dd5b
shadia shirin v <vshadiyashirin2003@gmail.com>
2025-03-16 15:10:55 +0530

    signup-login-cmplted1

--- reg/models.py
+++ reg/models.py
@@ -4,19 +4,19 @@ from users.models import *
 
 
 class Product(models.Model):
-        customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-        product_name = models.CharField(max_length=255)
-        model_number = models.CharField(max_length=100)
-        purchase_date = models.DateField()
-        warranty_status = models.IntegerField(help_text="Warranty period in months")
-        def __str__(self):
-            return self.product_name
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, to_field='user')
+    product_name = models.CharField(max_length=255)
+    model_number = models.CharField(max_length=100)
+    purchase_date = models.DateField()
+    warranty_status = models.IntegerField(help_text="Warranty period in months")
+    def __str__(self):
+        return self.product_name
         
 
 
 class Service(models.Model):
-        service = models.ForeignKey(ServiceCenter, on_delete=models.CASCADE)
-        service_catalog = models.CharField(max_length=1000,blank=True)
-        contact_no = models.CharField(max_length=10,blank=True,null=True)
-        def __str__(self):
-            return self.service
+    service = models.ForeignKey(ServiceCenter, on_delete=models.CASCADE)
+    service_catalog = models.CharField(max_length=1000,blank=True)
+    contact_no = models.CharField(max_length=10,blank=True,null=True)
+    def __str__(self):
+        return self.service
