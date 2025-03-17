9e21ea0fc0dcd63bc646516eb2d46d8bf2726d35
Zenha123 <fathimazenha21@gmail.com>
2025-03-17 14:34:31 +0530

    str function chnged

--- users/models.py
+++ users/models.py
@@ -44,7 +44,7 @@ class Customer(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.name + str(self.id)
+        return self.name
     
 class ServiceCenter(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE,primary_key=True)
