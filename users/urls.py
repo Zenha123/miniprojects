54a1e1ad3fe654fb19016cdbd7695972103a79d2
Zenha123 <fathimazenha21@gmail.com>
2025-03-17 11:22:47 +0530

    urls changedd

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('login/',views.user_login,name='login'),
     path('signup/',views.signup,name='signup'),
     path('verify-otp/', views.verify_otp, name='verify_otp'),
